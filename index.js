857a94ee6cc30a584dcd84066559a6b025f662eb
Jose De La Cruz <jldlcv@gmail.com>
2022-07-17 13:02:14 -0500

    exercise 3.16 phonebook database step4

--- index.js
+++ index.js
@@ -1,42 +1,11 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
+// Important that dotenv is imported before the model person
+require("dotenv").config();
 const cors = require("cors");
+const morgan = require("morgan");
 const PORT = process.env.PORT;
 const Person = require("./models/person")
-
-let persons = [
-	{
-		id: 1,
-		name: "Arto Hellas",
-		number: "040-123456",
-	},
-	{
-		id: 2,
-		name: "Ada Lovelace",
-		number: "39-44-5323523",
-	},
-	{
-		id: 3,
-		name: "Dan Abramov",
-		number: "12-43-234345",
-	},
-	{
-		id: 4,
-		name: "Mary Poppendieck",
-		number: "39-23-6423122",
-	},
-];
-
-// Activate the json-parser and implement an initial handler for dealing with the HTTP POST requests
-app.use(express.json());
-
-// Middleware to use and allow for requests from all origins
-app.use(cors());
-
-// To make express show static content (index.html, style.css and the JavaScript, etc.)
-app.use(express.static("build"));
 
 // Configure morgan so that it also shows the data sent in HTTP POST requests:
 morgan.token("data", (request, response) => {
@@ -49,6 +18,15 @@ app.use((request, response, next) => {
 });
 
 app.use(morgan(`:method :url :res[content-length] - :response-time ms :data`));
+
+// To make express show static content (index.html, style.css and the JavaScript, etc.)
+app.use(express.static("build"));
+
+// Activate the json-parser and implement an initial handler for dealing with the HTTP POST requests
+app.use(express.json());
+
+// Middleware to use and allow for requests from all origins
+app.use(cors());
 
 // Get main path
 app.get("/", (request, response) => {
@@ -67,6 +45,7 @@ app.post("/api/persons", (request, response) => {
 	const body = request.body;
 	
 	if (!body.name || !body.number) {
+		// 400 bad request
 		return response.status(400).json({
 			error: "missing name and/or number",
 		});
@@ -105,18 +84,41 @@ app.get("/api/persons/:id", (request, response) => {
 	Person
 		.findById(request.params.id)
 		.then(person => {
-			response.json(person);
-		});
+			if (person) {
+				response.json(person);
+			} else {
+				// 404 not found
+				response.status(404).end();
+			}
+		})
+		.catch(error => next(error));
 });
 
 // Delete person by id
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
 	Person.findByIdAndRemove(request.params.id)
 		.then(result => {
+			// 204 no content
 			response.status(204).end();
 		})
 		.catch(error => next(error));
 });
+
+// Set error handling into middleware
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message);
+
+	// If the error was caused by an invalid object id for Mongo.
+	if (error.name === "CastError") {
+		return response.status(400).send({ error: "formatted id" });
+	}
+
+	// Middleware passes the error forward to the default Express error handler
+	next(error);
+};
+
+// Handler of requests with result to errors
+app.use(errorHandler);
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
